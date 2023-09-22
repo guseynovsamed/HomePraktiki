f66ae090b1ddce01a5ce5d87401d62d76415deb2
guseynovsamed <samadaghash@code.edu.az>
2023-09-23 01:25:13 +0400

    HomeworkPraktiki

---
+++ HomeworkPraktiki2/Program.cs
@@ -0,0 +1,202 @@
+﻿
+
+
+
+
+
+//static void Test(int num2 , string name , int number=100)
+//{
+//    Console.WriteLine(num2+ name+number);
+//}
+
+
+//Test(10,"salam");
+
+
+
+//static void Test(int a, params int[] b)
+//{
+//    int sum = 0;
+//    foreach (var item in b)
+//    {
+//        sum += item;
+//    }
+
+//    int result = sum *a ;
+
+
+
+//    Console.WriteLine(result);
+//}
+
+
+
+
+//Test(1, 2, 3, 4, 5);
+
+
+
+
+
+//static void Test(int b , params int[] numbers)
+//{
+//    Console.WriteLine("Hello");
+//}
+
+//Test(1, 3, 4, 5, 6);
+
+
+
+//int a = 100;
+
+//int b = a;
+//b = 200;
+
+
+//bool test = true;
+
+
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+//string name = "test";
+
+//string surname = name;
+
+//surname = "salamlar";
+
+//Console.WriteLine(name);
+//Console.WriteLine(surname);
+
+
+
+
+//int[] arr1 = { 1, 2, 3 };
+
+//int[] arr2 = arr1;
+//arr2[0] = 300;
+
+
+//Console.WriteLine(arr1[0]);
+//Console.WriteLine(arr2[0]);
+
+
+
+
+//int[] nums = { 2, 3, 4, 5 };
+
+
+//Console.WriteLine(nums[0]);
+
+//Test(nums);
+
+
+//Console.WriteLine(nums[0]);
+
+
+
+//static void Test(int[] arr)
+//{
+//    arr[0] = 5;
+//    Console.WriteLine(arr[0]+10);
+//}
+
+
+//int b = 20;
+
+
+//Console.WriteLine(b);
+
+//Test(ref b);
+
+//Console.WriteLine(b);
+
+
+
+//static void Test(ref int a)
+//{
+//    a = 200;
+
+//    Console.WriteLine(a);
+//}
+
+
+
+
+
+
+
+//int b = 20;
+
+
+//Console.WriteLine(b);
+
+
+
+
+//Test(out b);
+
+//Console.WriteLine(b);
+
+
+
+//static void Test(out int a)
+//{
+//    a = 200;
+
+//    Console.WriteLine(a);
+//}
+
+
+
+
+//int a = 5;
+
+//int b = ++a;
+
+
+//Console.WriteLine(a);
+
+//Console.WriteLine(b);
+
+
+
+//int a = 5;
+//int c = 10;
+
+//int b = ++a;
+
+//a++;
+
+//c = --a;
+
+
+//Console.WriteLine(a);
+
+//Console.WriteLine(b);
+
+//Console.WriteLine(c);
+
+
+//int[] nums1 = {2,3,4,5};
+
+//int a = nums1[0];
+
+//int b = a++;
+
+//int[] nums2 = nums1;
+
+//nums2[0] = a;
+
+//Test(nums2);
+
+//Console.WriteLine(nums1[0]);
+
+//static void Test(int[] arr)
+//{
+//    arr[0] = 50;
+//}
+
+
